298c7ba6115c9ecff4e95b0b7e241af7a096800a
RADHA THAKARE <67684749+radhathakare@users.noreply.github.com>
2023-05-22 15:47:51 +0530

    Update DocumnetDisplay.js

--- src/Components/DocumnetDisplay.js
+++ src/Components/DocumnetDisplay.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import './DocumnetDisplay.css'
-
 import { TfiMenuAlt } from 'react-icons/tfi';
 
-const DocumentDisplay = ({ content, formattingOptions }) => {
-  const getStyle = () => {
-    let style = {};
-    if (formattingOptions.bold) {
-      style = { ...style, fontWeight: 'bold' };
-    }
-    if (formattingOptions.italic) {
-      style = { ...style, fontStyle: 'italic' };
-    }
-    return style;
-  };
+const DocumentDisplay = () => {
+
 
   return (
-    <div  style={getStyle()}>
+    <div>
         <TfiMenuAlt className='Displaydocumnet' size={23} />
      
     </div>
